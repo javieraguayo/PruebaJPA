0439609834bd86822d2c3dfadf206666ed38e263
javie <javie@DESKTOP-9K564OC.home>
2016-10-30 22:17:59 -0300

    prueba funcionando agregar usuario

---
+++ src/main/java/com/bushodevelopers/pruebajpa2/model/RolFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bushodevelopers.pruebajpa2.model;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author javie
+ */
+@Stateless
+public class RolFacade extends AbstractFacade<Rol> {
+
+    @PersistenceContext(unitName = "com.bushoDevelopers_pruebaJpa2.0_war_2.0PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public RolFacade() {
+        super(Rol.class);
+    }
+    
+}
